a95b2c16bf1201f07a4e82eb3686be66a56e5db9
murcoder <chris.murauer@yahoo.de>
2023-05-16 15:05:54 +0200

    init ionic project

---
+++ src/views/GalleryPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <ion-page>
+        <!--    <ion-header :translucent="true">-->
+        <!--      <ion-toolbar>-->
+        <!--        <ion-title>Web Applikation DER STEINBRUCH, DAS LAGER UND DIE ORTSCHAFTEN</ion-title>-->
+        <!--      </ion-toolbar>-->
+        <!--    </ion-header>-->
+
+        <ion-content :fullscreen="true">
+            <!--      <ion-header collapse="condense">-->
+            <!--        <ion-toolbar>-->
+            <!--          <ion-title size="large">Der Steinbruch</ion-title>-->
+            <!--        </ion-toolbar>-->
+            <!--      </ion-header>-->
+
+        </ion-content>
+    </ion-page>
+</template>
+
+<script setup lang="ts">
+import {IonNav, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,106 @@
+<template>
+    <ion-page>
+        <!--    <ion-header :translucent="true">-->
+        <!--      <ion-toolbar>-->
+        <!--        <ion-title>Web Applikation DER STEINBRUCH, DAS LAGER UND DIE ORTSCHAFTEN</ion-title>-->
+        <!--      </ion-toolbar>-->
+        <!--    </ion-header>-->
+
+        <ion-content :fullscreen="true">
+            <!--      <ion-header collapse="condense">-->
+            <!--        <ion-toolbar>-->
+            <!--          <ion-title size="large">Der Steinbruch</ion-title>-->
+            <!--        </ion-toolbar>-->
+            <!--      </ion-header>-->
+
+            <div id="container">
+                <ion-row>
+                    <ion-text color="white" size="--ion-font-size" class="ion-margin-end">
+                        <h1>Web Applikation DER STEINBRUCH, <br>DAS LAGER UND DIE ORTSCHAFTEN</h1>
+                    </ion-text>
+                </ion-row>
+                <ion-row>
+                    <ion-list lines="none">
+                        <ul class="navigation-list mt-6">
+                            <li>
+                                <ion-icon color="white" :icon="globe()" class="mr-2 font-bold"></ion-icon>
+                                <strong>
+                                    Mit GPS Empfang
+                                </strong>
+                            </li>
+                            <li>
+                                <ion-icon color="white" :icon="image()" class="mr-2 font-bold"></ion-icon>
+                                <strong>Ersatzmodus ohne GPS Empfang</strong>
+                            </li>
+                            <li>
+                                <ion-icon color="white" :icon="documentText()" class="mr-2 font-bold"></ion-icon>
+                                <strong>Projektbeschreibung und Quellen</strong>
+                            </li>
+                        </ul>
+                    </ion-list>
+                </ion-row>
+
+            </div>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script lang="ts">
+import {IonNav, IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
+import GalleryPage from './GalleryPage.vue';
+import {documentText, globe, image} from "ionicons/icons";
+
+export default {
+    methods: {
+        globe() {
+            return globe
+        },
+        image() {
+            return image
+        },
+        documentText() {
+            return documentText
+        }
+    },
+    components: {IonNav, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, documentText, globe, image},
+    data() {
+        return {
+            component: GalleryPage,
+        };
+    },
+};
+</script>
+
+<style scoped>
+#container {
+    padding: 1em;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+#container strong {
+    font-size: 20px;
+    line-height: 26px;
+}
+
+#container p {
+    font-size: 16px;
+    line-height: 22px;
+
+    color: #8c8c8c;
+
+    margin: 0;
+}
+
+#container a {
+    text-decoration: none;
+}
+
+.navigation-list {
+    list-style: none;
+    padding: 0;
+}
+</style>
