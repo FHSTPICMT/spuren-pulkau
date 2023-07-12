65268d854bd290036154fd1deeeca188fe9e0026
murcoder <chris.murauer@yahoo.de>
2023-07-12 22:34:24 +0200

    * added coordinates

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -13,7 +13,8 @@ export const TRACKS_NO_GPS = [
   },
   {
     audio: 'src/assets/audio/03_Verladestation.mp3',
-    image: 'src/assets/img/03_Verladestation.jpg'
+    image: 'src/assets/img/03_Verladestation.jpg',
+    coords: "48.687997,15.853439"
   },
   {
     audio: 'src/assets/audio/04_Das_Lager.mp3',
@@ -21,7 +22,8 @@ export const TRACKS_NO_GPS = [
   },
   {
     audio: 'src/assets/audio/04_Die_Verladerampe.mp3',
-    image: 'src/assets/img/04_Die_Verladerampe.jpg'
+    image: 'src/assets/img/04_Die_Verladerampe.jpg',
+    coords: "48.687936,15.853981"
   },
   {
     audio: 'src/assets/audio/05_Die_Ortschaften.mp3',
@@ -37,7 +39,8 @@ export const TRACKS_NO_GPS = [
   },
   {
     audio: 'src/assets/audio/08_Die_Wirtschaft.mp3',
-    image: 'src/assets/img/08_Die_Wirtschaft.jpg'
+    image: 'src/assets/img/08_Die_Wirtschaft.jpg',
+    coords: "48.687835,15.852429"
   },
   {
     audio: 'src/assets/audio/09_Die_Kollegen.mp3',
@@ -117,7 +120,8 @@ export const TRACKS_NO_GPS = [
   },
   {
     audio: 'src/assets/audio/28_Die_Schotterwerbung.mp3',
-    image: 'src/assets/img/28_Die_Schotterwerbung.jpg'
+    image: 'src/assets/img/28_Die_Schotterwerbung.jpg',
+    ccords: "48.687626,15.853462"
   },
   {
     audio: 'src/assets/audio/29_Der_Badeteich.mp3',
@@ -160,7 +164,8 @@ export const TRACKS = [
   },
   {
     audio: 'src/assets/audio/01_Die_Ortsansaessigen.mp3',
-    image: 'src/assets/img/01_Die_Ortsansaessigen.jpg'
+    image: 'src/assets/img/01_Die_Ortsansaessigen.jpg',
+    coords: "48.688178,15.852417",
   },
   {
     audio: 'src/assets/audio/02_Steinbruch_Ort.mp3',
@@ -180,7 +185,8 @@ export const TRACKS = [
   },
   {
     audio: 'src/assets/audio/05_Die_Ortschaften.mp3',
-    image: 'src/assets/img/05_Die_Ortschaften.jpg'
+    image: 'src/assets/img/05_Die_Ortschaften.jpg',
+    coords: "48.687775,15.852960"
   },
   {
     audio: 'src/assets/audio/06_Die_Kirche.mp3',
@@ -188,7 +194,8 @@ export const TRACKS = [
   },
   {
     audio: 'src/assets/audio/07_Die_Einfahrt.mp3',
-    image: 'src/assets/img/07_Die_Einfahrt.jpg'
+    image: 'src/assets/img/07_Die_Einfahrt.jpg',
+    coords: "48.688121,15.852941",
   },
   {
     audio: 'src/assets/audio/08_Die_Wirtschaft.mp3',
@@ -196,67 +203,83 @@ export const TRACKS = [
   },
   {
     audio: 'src/assets/audio/09_Die_Kollegen.mp3',
-    image: 'src/assets/img/09_Die_Kollegen.jpg'
+    image: 'src/assets/img/09_Die_Kollegen.jpg',
+    coords: "48.686426,15.855102"
   },
   {
     audio: 'src/assets/audio/10_Das_Spalten.mp3',
-    image: 'src/assets/img/10_Das_Spalten.jpg'
+    image: 'src/assets/img/10_Das_Spalten.jpg',
+    coords: "48.686426,15.855102"
   },
   {
     audio: 'src/assets/audio/11_Der_Baron.mp3',
-    image: 'src/assets/img/11_Der_Baron.jpg'
+    image: 'src/assets/img/11_Der_Baron.jpg',
+    coords: "48.687626,15.853462"
   },
   {
     audio: 'src/assets/audio/12_Die_Betriebsbaracke.mp3',
-    image: 'src/assets/img/12_Die_Betriebsbaracke.jpg'
+    image: 'src/assets/img/12_Die_Betriebsbaracke.jpg',
+    coords: "48.686014,15.853445"
   },
   {
     audio: 'src/assets/audio/13_Das_Ziegelwerk.mp3',
-    image: 'src/assets/img/13_Das_Ziegelwerk.jpg'
+    image: 'src/assets/img/13_Das_Ziegelwerk.jpg',
+    coords: "48.687592,15.854011"
   },
   {
     audio: 'src/assets/audio/14_Das_Waechterhaus.mp3',
-    image: 'src/assets/img/14_Das_Waechterhaus.jpg'
+    image: 'src/assets/img/14_Das_Waechterhaus.jpg',
+    coords: "48.686724,15.853362"
   },
   {
     audio: 'src/assets/audio/15_Das_Steinbrecherhaus_Betrieb.mp3',
-    image: 'src/assets/img/15_Das_Steinbrecherhaus_Betrieb.jpg'
+    image: 'src/assets/img/15_Das_Steinbrecherhaus_Betrieb.jpg',
+    coords: "48.687077,15.853421"
   },
   {
     audio: 'src/assets/audio/16_Das_Steinbrecherhaus_Stilllegung.mp3',
-    image: 'src/assets/img/16_Das_Steinbrecherhaus_Stilllegung.jpg'
+    image: 'src/assets/img/16_Das_Steinbrecherhaus_Stilllegung.jpg',
+    coords: "48.687077,15.853421"
   },
   {
     audio: 'src/assets/audio/17_Das_NS-Arbeitsbuch_des_Steinbrucharbeiters.mp3',
-    image: 'src/assets/img/17_Das_NS-Arbeitsbuch_des_Steinbrucharbeiters.jpg'
+    image: 'src/assets/img/17_Das_NS-Arbeitsbuch_des_Steinbrucharbeiters.jpg',
+    coords: "48.685856,15.854571"
   },
   {
     audio: 'src/assets/audio/18_Der_Gefolgschaftsraum.mp3',
-    image: 'src/assets/img/18_Der_Gefolgschaftsraum.jpg'
+    image: 'src/assets/img/18_Der_Gefolgschaftsraum.jpg',
+    coords: "48.686369,15.853397"
   },
   {
     audio: 'src/assets/audio/19_Die_Zwangsarbeit.mp3',
-    image: 'src/assets/img/19_Die_Zwangsarbeit.jpg'
+    image: 'src/assets/img/19_Die_Zwangsarbeit.jpg',
+    coords: "48.686072,15.855005"
   },
   {
     audio: 'src/assets/audio/20_Der_Betriebsfuehrer.mp3',
-    image: 'src/assets/img/20_Der_Betriebsfuehrer.jpg'
+    image: 'src/assets/img/20_Der_Betriebsfuehrer.jpg',
+    coords: "48.686369,15.853397"
   },
   {
     audio: 'src/assets/audio/21_Der_Nahrungsmangel.mp3',
-    image: 'src/assets/img/21_Der_Nahrungsmangel.jpg'
+    image: 'src/assets/img/21_Der_Nahrungsmangel.jpg',
+    coords: "48.686014,15.853445"
   },
   {
     audio: 'src/assets/audio/22_Der_Kellerausbau.mp3',
-    image: 'src/assets/img/22_Der_Kellerausbau.jpg'
+    image: 'src/assets/img/22_Der_Kellerausbau.jpg',
+    coords: "48.686072,15.855005"
   },
   {
     audio: 'src/assets/audio/23_Der_Aufseher.mp3',
-    image: 'src/assets/img/23_Der_Aufseher.jpg'
+    image: 'src/assets/img/23_Der_Aufseher.jpg',
+    coords: "48.686724,15.853362"
   },
   {
     audio: 'src/assets/audio/24_Das_Lager_im_Westen.mp3',
-    image: 'src/assets/img/24_Das_Lager_im_Westen.jpg'
+    image: 'src/assets/img/24_Das_Lager_im_Westen.jpg',
+    coords: "48.685856,15.854571"
   },
   {
     audio: 'src/assets/audio/25_Vor_der_Befreiung.mp3',
@@ -264,47 +287,58 @@ export const TRACKS = [
   },
   {
     audio: 'src/assets/audio/26_Tag_der_Befreiung.mp3',
-    image: 'src/assets/img/26_Tag_der_Befreiung.jpg'
+    image: 'src/assets/img/26_Tag_der_Befreiung.jpg',
+    coords: "48.685660,15.853467"
   },
   {
     audio: 'src/assets/audio/27_Denunziert_und_deportiert.mp3',
-    image: 'src/assets/img/27_Denunziert_und_deportiert.jpg'
+    image: 'src/assets/img/27_Denunziert_und_deportiert.jpg',
+    coords: "48.687592,15.854011"
   },
   {
     audio: 'src/assets/audio/28_Die_Schotterwerbung.mp3',
-    image: 'src/assets/img/28_Die_Schotterwerbung.jpg'
+    image: 'src/assets/img/28_Die_Schotterwerbung.jpg',
+    coords: "48.687626,15.853462"
   },
   {
     audio: 'src/assets/audio/29_Der_Badeteich.mp3',
-    image: 'src/assets/img/29_Der_Badeteich.jpg'
+    image: 'src/assets/img/29_Der_Badeteich.jpg',
+    coords: "48.687012,15.854614"
   },
   {
     audio: 'src/assets/audio/30_Der_Badeteich_und_das_Steinbrecherhaus.mp3',
-    image: 'src/assets/img/30_Der_Badeteich_und_das_Steinbrecherhaus.jpg'
+    image: 'src/assets/img/30_Der_Badeteich_und_das_Steinbrecherhaus.jpg',
+    coords: "48.687120,15.853970"
   },
   {
     audio: 'src/assets/audio/31_Der_Badeunfall.mp3',
-    image: 'src/assets/img/31_Der_Badeunfall.jpg'
+    image: 'src/assets/img/31_Der_Badeunfall.jpg',
+    coords: "48.687012,15.854614"
   },
   {
     audio: 'src/assets/audio/32_Die_Austrocknung_des_Teichs.mp3',
-    image: 'src/assets/img/32_Die_Austrocknung_des_Teichs.jpg'
+    image: 'src/assets/img/32_Die_Austrocknung_des_Teichs.jpg',
+    coords: "48.686785,15.855027"
   },
   {
     audio: 'src/assets/audio/33_Der_Landschaftsgarten.mp3',
-    image: 'src/assets/img/33_Der_Landschaftsgarten.jpg'
+    image: 'src/assets/img/33_Der_Landschaftsgarten.jpg',
+    coords: "48.687339,15.854404"
   },
   {
     audio: 'src/assets/audio/34_Die_Passionsspiele.mp3',
-    image: 'src/assets/img/34_Die_Passionsspiele.jpg'
+    image: 'src/assets/img/34_Die_Passionsspiele.jpg',
+    coords: "48.687339,15.854404"
   },
   {
     audio: 'src/assets/audio/35_Die_Wehrsportuebungen.mp3',
-    image: 'src/assets/img/35_Die_Wehrsportuebungen.jpg'
+    image: 'src/assets/img/35_Die_Wehrsportuebungen.jpg',
+    coords: "48.685348,15.853703"
   },
   {
     audio: 'src/assets/audio/36_Rechtsradikale_Umtriebe.mp3',
-    image: 'src/assets/img/36_Rechtsradikale_Umtriebe.jpg'
+    image: 'src/assets/img/36_Rechtsradikale_Umtriebe.jpg',
+    coords: "48.685348,15.853703"
   }
 ]
 
